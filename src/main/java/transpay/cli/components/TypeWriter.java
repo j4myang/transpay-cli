c5c24ad4188b44a3fae123098cdca6f936f84c16
j4myang <jamyang.dev@gmail.com>
2024-12-19 03:57:57 +0800

    Remove unused constructor

--- src/main/java/transpay/cli/components/TypeWriter.java
+++ src/main/java/transpay/cli/components/TypeWriter.java
@@ -29,34 +29,4 @@ public class TypeWriter {
             ConsoleLog.print("\n");
         }
     }
-
-    public TypeWriter(Log logType, String text, int milliDelay, boolean newLine) {
-        for (char c : text.toCharArray()) {
-            String character = String.valueOf(c);
-
-            try {
-                switch (logType) {
-                    case ERROR -> ConsoleLog.logError(character);
-                    case SUCCESS -> ConsoleLog.logSuccess(character);
-                    case INFO -> ConsoleLog.logInfo(character);
-                    case BODY -> ConsoleLog.logBody(character);
-                    case HEADING -> ConsoleLog.logHeading(character);
-                    case OPTION -> ConsoleLog.logOption(character);
-                    case INPUT -> ConsoleLog.logInput(character);
-                    case SYSTEM -> ConsoleLog.logSystem(character);
-                }
-
-                Thread.sleep(milliDelay);
-            } catch (InterruptedException e) {
-                ConsoleLog.logError("TypeWriter aborted. Thread interrupted.");
-                e.printStackTrace();
-                break;
-            }
-        }
-
-        if (newLine) {
-            ConsoleLog.print("\n");
-        }
-    }
-
 }
